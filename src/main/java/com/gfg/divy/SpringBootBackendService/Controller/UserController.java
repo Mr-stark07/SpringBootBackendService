98900d5b2e30682b6c4ad354986f6d2ea1981d51
Mr-stark07 <divyaprakash_5@hotmail.com>
2019-07-27 18:07:09 +0530

    crud operation done

--- src/main/java/com/gfg/divy/SpringBootBackendService/Controller/UserController.java
+++ src/main/java/com/gfg/divy/SpringBootBackendService/Controller/UserController.java
@@ -3,57 +3,93 @@ package com.gfg.divy.SpringBootBackendService.Controller;
 import com.gfg.divy.SpringBootBackendService.Model.User;
 import com.gfg.divy.SpringBootBackendService.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@RestController
-public class UserController {
+@Controller
+public class UserController implements ErrorController{
 
     @Autowired
     UserService userService;
 
+    private static final String PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+    @RequestMapping("/error")
+    public String error(HttpServletRequest request, Model model){
+//        String uri = request.getRequestURI();
+//        model.addAttribute("error","Something went wrong");
+        return "error";
+    }
+
     @GetMapping("/")
-    public String root(){
-        return "Welcome to Spring.";
+    public String root(Model model){
+        model.addAttribute("users",userService.findAllUsers());
+        return "welcome";
     }
 
-    @GetMapping("/createUser")
-    public String createUser(){
-        return "CreateUser";
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("user",new User());
+        return "create";
     }
 
-    @GetMapping("/users")
-    public List<User> getAll(){
-        return userService.findAllUsers();
-    }
-
-    @GetMapping("/user/{id}")
-    public User getById(@PathVariable("id") Long id){
-        return userService.getUserById(id);
-    }
+//    @GetMapping("/users")
+//    public List<User> getAll(){
+//        return userService.findAllUsers();
+//    }
+//
+//    @GetMapping("/user/{id}")
+//    public User getById(@PathVariable("id") Long id){
+//        return userService.getUserById(id);
+//    }
 
     @PostMapping("/user")
-    public String create(@RequestBody User user){
+    public String create(@ModelAttribute User user){
         User user1 = userService.createUser(user);
-        if(user1 != null)
-            return "User created successfully.";
-        return "Something went wrong";
+        if(user1 != null){
+            return "redirect:/";
+        }
+        return "redirect:/create";
     }
 
-    @PutMapping("/user")
-    public String update(@RequestBody User user){
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id, Model model){
+        model.addAttribute("user",userService.getUserById(id));
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute User user){
         User user1 = userService.updateUser(user);
         if(user1 != null)
-            return "User updated successfully.";
-        return "Something went wrong";
+            return "redirect:/";
+        return "error";
     }
 
-    @DeleteMapping("/user/{id}")
+    @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id){
         User user1 = userService.deleteUser(id);
         if(user1 != null)
-            return "User deleted successfully.";
-        return "Something went wrong";
+            return "redirect:/";
+        return "error";
     }
+//
+//
+//    @GetMapping("/userInfo")
+//    public String userInfo(){
+//        RestTemplate restTemplate = new RestTemplate();
+//        return restTemplate.getForObject("http://codeforces.com/api/user.info?handles=DmitriyH;Fefer_Ivan", String.class);
+//    }
 }
--- src/main/java/com/gfg/divy/SpringBootBackendService/Model/User.java
+++ src/main/java/com/gfg/divy/SpringBootBackendService/Model/User.java
@@ -18,9 +18,11 @@ public class User {
     private String first_name;
     @Getter @Setter
     private String last_name;
+    @Getter @Setter
+    private String email;
 
-    public User(String first_name, String last_name) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-    }
+//    public User(String first_name, String last_name) {
+//        this.first_name = first_name;
+//        this.last_name = last_name;
+//    }
 }
